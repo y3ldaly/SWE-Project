b64603c17a4d6468cbcc7a03e6105ffeaee2f881
Youssef Eldaly <youssef.h.eldaly@gmail.com>
2024-05-07 10:38:59 -0400

    finished rest of slides

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,26 +1,11 @@
-// import Order from './order/Order'
-// import MenuPage from './menuPage/firstPage/MenuPage'
-// import Appetizers from './menuPage/fullMenu/appetizers/Appetizers'
-// import MainDishes from './menuPage/fullMenu/mainDishes/mainDishes'
-// import SignatureDishes from './menuPage/fullMenu/signatureDishes/SignatureDishes'
-// import FullMenu from './menuPage/fullMenu/fullMenu'
-// import PaymentPage from './paymentPage/paymentPage'
-// import LoginForm from './Login Page/LoginForm'
-// import CustomerRegistrationForm from './Register Page/CustomerRegForm'; // Adjust the path as necessary
-// import ManagerRegistrationForm from './Register Page/ManagerRegForm';
-// import AddDeposit from "./insufficient funds/addDepoist"
-// import InsufficientFunds from "./insufficient funds/insufficient"
-// import HomeScreen from './HomeScreen/HomeScreen'
-// import VIPUser from './HomeScreen/VIPHomeScreen'
-// import AddDeposit from './insufficient funds/addDepoist'
-// import Complaints from "./Complaints/Urgent"
-import Important from './Complaints/Important'
-// use this file to make sure your components work
+
+
 
 function App() {
     return ( 
-          <Important/>
-
+        <>
+            
+        </>
     )
 }
 
---
+++ my-react-app/src/users/registered/pages/UserDisputeComplaints.jsx
@@ -0,0 +1,14 @@
+import Form from "../components/Form"
+import UserNavbar from '../components/UserNavbar' 
+
+function UserDisputeComplaints() {
+    return(
+        <>
+            <UserNavbar/>
+            <Form caption="Dispute this comment here. Identify why you think you don’t deserve this criticism:" title="" option1="" option2="" option3=""/>
+            
+        </>
+    )
+}
+
+export default UserDisputeComplaints
