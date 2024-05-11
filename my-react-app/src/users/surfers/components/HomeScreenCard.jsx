6921e88729cc5ddc7f93702114344972af1fc218
Youssef Eldaly <youssef.h.eldaly@gmail.com>
2024-05-11 11:41:35 -0400

    message

--- my-react-app/src/users/surfers/components/HomeScreenCard.jsx
+++ my-react-app/src/users/surfers/components/HomeScreenCard.jsx
@@ -1,19 +1,29 @@
-import '../pages/homeScreen/UserHomeScreen.css'
-
+// import '../pages/homeScreen/UserHomeScreen.css'
+import PropTypes from 'prop-types';
 
 function HomeScreenCard(props) {
-    return(
-        <div class="card">
-            <img src={props.img} alt={props.altText}/>
-            <h3 class="foodcard-title">{props.title}</h3>
-            <p class="foodcard-caption">{props.caption}</p>
-            <div class="star-rating">
+    return (
+        <div className="card">
+            <img src={props.img} alt={props.altText} />
+            <h3 className="foodcard-title">{props.title}</h3>
+            <p className="foodcard-caption">{props.caption}</p>
+            <div className="star-rating">
                 {props.rating}
             </div>
-            <p class="foodcard-price">{props.price}</p>
-            <button class="order-button">ORDER</button>
+            <p className="foodcard-price">{props.price}</p>
+            <button className="order-button">ORDER</button>
         </div>
-    )
+    );
 }
 
-export default HomeScreenCard
+// Define propTypes for type checking
+HomeScreenCard.propTypes = {
+    img: PropTypes.string.isRequired, // img should be a string and is required
+    altText: PropTypes.string.isRequired, // altText should be a string and is required
+    title: PropTypes.string.isRequired, // title should be a string and is required
+    caption: PropTypes.string.isRequired, // caption should be a string and is required
+    rating: PropTypes.node, // rating can be any type of React node
+    price: PropTypes.string.isRequired, // price should be a string and is required
+};
+
+export default HomeScreenCard;
